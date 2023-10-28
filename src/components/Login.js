3b29d5830a244b938b0caf651fd79fea7f11c81f
AbhayShaw0301 <abhayshaw0301@gmail.com>
2023-10-28 15:16:34 +0530

    refactored isValdiate file and created auth logic

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,48 +1,116 @@
-import React, { useRef, useState } from 'react';
-import { checkValidateForm } from '../utils/isValidate';
-import Header from './Header';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import React, { useRef, useState } from "react";
+import { auth } from "../utils/firebase";
+import { checkValidData } from "../utils/isValidate";
+import Header from "./Header";
 
 const Login = () => {
-    const [isSignInForm, setSignInForm] = useState(true);
-    const [errorMessage, setErrorMessage] = useState(null);
-    const name=useRef(null);
-    const email =useRef(null);
-    const password =useRef(null);
-    const handleButtonClick =()=>{
-      //Validating the Form
-      
-        const message=  checkValidateForm(email.current.value,password.current.value,name.current.value)
-        setErrorMessage(message);
+  const [isSignInForm, setSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const email = useRef(null);
+  const password = useRef(null);
+  const handleButtonClick = () => {
+    //Validating the Form
 
-    //Upon Successful Validation performing Sign In / Sign Up
-
-
+    const message = checkValidData(
+      email.current.value,
+      password.current.value,
+    );
+    setErrorMessage(message);
+    if (message) return;
+    if (!isSignInForm) {
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          console.log(user);
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+          // ..
+        });
+    } else {
+      signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode+"-"+errorMessage);
+        });
     }
 
-    const toggleSignInForm = ()=>{
-        
-        setSignInForm(!isSignInForm)
-    }
+
+   
+  };
+
+  const toggleSignInForm = () => {
+    setSignInForm(!isSignInForm);
+  };
   return (
-   <>
-   <Header/>
-   <div className='absolute'>
-   <img src='https://assets.nflxext.com/ffe/siteui/vlv3/dace47b4-a5cb-4368-80fe-c26f3e77d540/f5b52435-458f-498f-9d1d-ccd4f1af9913/IN-en-20231023-popsignuptwoweeks-perspective_alpha_website_large.jpg'
-    alt='logo'/> 
-   </div>
-   <form onSubmit={(e)=>e.preventDefault()} className='absolute w-3/12 p-12  my-36 mx-auto right-0 left-0 bg-black text-white rounded-lg bg-opacity-80'>
-    <h1 className='font-bold text-3xl py-4'>{isSignInForm ? 'Sign In':'Sign Up'}</h1>
-    {!isSignInForm && (<input ref={name} type='text' placeholder='Name' className='p-4 my-4 w-full bg-gray-700'/>)}
-    <input ref={email} type='text' placeholder='Email Address' className='p-4 my-4 w-full bg-gray-700'/>
-    <input  ref={password} type='password' placeholder='Password' className='p-4 my-4 w-full bg-gray-700'/>
-    <p className='text-red-600 font-bold text-lg py-2'>{errorMessage}</p>
-    <button onClick={handleButtonClick} className='p-4 my-6 bg-red-700  w-full rounded-lg'  > {isSignInForm ? 'Sign In':'Sign Up'}</button>
+    <>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/dace47b4-a5cb-4368-80fe-c26f3e77d540/f5b52435-458f-498f-9d1d-ccd4f1af9913/IN-en-20231023-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="logo"
+        />
+      </div>
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="absolute w-3/12 p-12  my-36 mx-auto right-0 left-0 bg-black text-white rounded-lg bg-opacity-80"
+      >
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            // ref={name}
+            type="text"
+            placeholder="Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
+        <input
+          ref={email}
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <input
+          ref={password}
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <p className="text-red-600 font-bold text-lg py-2">{errorMessage}</p>
+        <button
+          onClick={handleButtonClick}
+          className="p-4 my-6 bg-red-700  w-full rounded-lg"
+        >
+          {" "}
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
 
-    <p className='py-4 cursor-pointer'onClick={toggleSignInForm}>{isSignInForm ? 'New to Netflix? Sign Up Now.':'Already registered. Sign In Now.'}</p>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now."
+            : "Already registered. Sign In Now."}
+        </p>
+      </form>
+    </>
+  );
+};
 
-   </form>
-   </>
-  )
-}
-
-export default Login
+export default Login;
--- src/utils/isValidate.js
+++ src/utils/isValidate.js
@@ -1,10 +1,12 @@
-export const checkValidateForm = (name,email,password)=>{
-    const isValidName =/^[A-Z][a-zA-Z '.-]*[A-Za-z][^-]$/.test(name);
-    const isValidEmail =/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
-const isValidPassword=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
-if(!isValidName) return "Invalid Username."
-if(!isValidEmail) return "Invalid email address."
-if(!isValidPassword) return "Password is not valid."
+export const checkValidData = (email, password) => {
+  const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(
+    email
+  );
+  const isPasswordValid =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
 
-return null;
-}
+  if (!isEmailValid) return "Email ID is not valid";
+  if (!isPasswordValid) return "Password is not valid";
+
+  return null;
+};
