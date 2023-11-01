7c3213e43f11626efb0f2929cd5252f2334b7a07
AbhayShaw0301 <abhayshaw0301@gmail.com>
2023-11-01 11:33:56 +0530

    fixed bugs related to MultiLanguage select

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,72 +1,88 @@
 import { onAuthStateChanged, signOut } from "firebase/auth";
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import logo from "../assets/images/logo.png";
 import { changeLanguage } from "../utils/configSlice";
 import { SUPPORTED_LANGUAGES } from "../utils/constant";
-import { auth } from '../utils/firebase';
+import { auth } from "../utils/firebase";
 import { toggleGptSearchView } from "../utils/gptSearchSlice";
-import { addUser, removeUser } from '../utils/userSlice';
+import { addUser, removeUser } from "../utils/userSlice";
 
 const Header = () => {
+const showGptSearch=useSelector((store)=>store.gpt.showGptSearch);
   const dispatch = useDispatch();
-  const user=useSelector(store=>store.user);
-  const navigate=useNavigate();
-  const handleSignOut=()=>{
-    signOut(auth).then(() => {
-      // Sign-out successful.
-    }).catch((error) => {
-      // An error happened.
-      console.error(error);
-    });
-  }
-  useEffect(()=>{
-    const unsubscribe=onAuthStateChanged(auth, (user) => {
+  const user = useSelector((store) => store.user);
+  const navigate = useNavigate();
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+        console.error(error);
+      });
+  };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
-        const {uid,email,displayName,photoURL} = user;
-        dispatch(addUser({uid,email,displayName,photoURL}));
-       navigate('/browse')
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(addUser({ uid, email, displayName, photoURL }));
+        navigate("/browse");
         // ...
       } else {
         // User is signed out
         // ...
         dispatch(removeUser());
-        navigate('/')
-        
+        navigate("/");
       }
-      return ()=>unsubscribe()
+      return () => unsubscribe();
     });
-
-  },[]);
-  const onToggleSearchClick=()=>{
+  }, []);
+  const onToggleSearchClick = () => {
     dispatch(toggleGptSearchView());
-    
-
-  }
-  const toggleLanguageChange=(e)=>{
-    dispatch(changeLanguage(e.target.value))
-  }
+  };
+  const toggleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  };
   return (
-  <div className='absolute py-2 px-8 bg-gradient-to-b from-black z-10 w-screen flex justify-between'>
-  <img className='w-44 ' src={logo} alt='logo'/>
-  {user && (<div className='flex'>
-  <select className='bg-gray-900 text-white text-xl m-2  p-2 rounded'onChange={toggleLanguageChange} >
-  {
-    SUPPORTED_LANGUAGES.map((lang)=> <option key={lang.identifier} value={lang.identifier}>{lang.name}</option>)
-  }
- 
-    
+    <div className="absolute py-2 px-8 bg-gradient-to-b from-black z-10 w-screen flex justify-between">
+      <img className="w-44 " src={logo} alt="logo" />
+      {user && (
+        <div className="flex">
+         {showGptSearch && (
 
-  </select>
+            <select
+              className="bg-gray-900 text-white text-xl m-2  p-2 rounded"
+              onChange={toggleLanguageChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+         )}
 
-    <button className='bg-purple-800 text-white text-xl m-2  px-12  rounded' onClick={onToggleSearchClick}>Gpt Search</button>
-    <img className='w-12 h-12 my-2 mx-2' src={user.photoURL} alt='userLogo'/>
-    <button className='font-bond text-white' onClick={handleSignOut}>Sign Out</button>
-  </div>)}
-  
-  </div>
-  )
-}
+          <button
+            className="bg-purple-800 text-white text-xl m-2  px-12  rounded"
+            onClick={onToggleSearchClick}
+          >{showGptSearch?'HomePage':'Gpt Search'}
+          
+          </button>
+          <img
+            className="w-12 h-12 my-2 mx-2"
+            src={user.photoURL}
+            alt="userLogo"
+          />
+          <button className="font-bond text-white" onClick={handleSignOut}>
+            Sign Out
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default Header
+export default Header;
