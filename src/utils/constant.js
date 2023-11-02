979ae907a021da9ae6db121117e561c19a8903f3
AbhayShaw0301 <abhayshaw0301@gmail.com>
2023-11-02 22:44:20 +0530

    secured keys adding env file

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,10 +3,10 @@ export const API_OPTIONS= {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOTIzYzJmMjhjZGU3YjBlNDI5NWM2OWI5YWZjN2ZhMiIsInN1YiI6IjY1MGZlMGI2YTNlNGJhMDBhYmVmMWExMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._Oz45uC8NOks4Bc86gbS21y_lpJ2bM_lZYeW_5sq0xE'
+      Authorization: 'Bearer'+process.env.REACT_APP_TMDB_KEY
     }
   };
   export const IMG_CDN_URL='https://image.tmdb.org/t/p/w500/'
   export const BG_URL='https://assets.nflxext.com/ffe/siteui/vlv3/dace47b4-a5cb-4368-80fe-c26f3e77d540/f5b52435-458f-498f-9d1d-ccd4f1af9913/IN-en-20231023-popsignuptwoweeks-perspective_alpha_website_large.jpg'
   export const SUPPORTED_LANGUAGES = [{identifier:'en',name:"English"},{identifier:'hindi',name:"Hindi"},{identifier:'spanish',name:"Spanish"},{identifier:'mandarin',name:"Mandarin"},{identifier:'latin',name:"Latin"},]
-  export const OPENAI_API_KEY='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'
+  export const OPENAI_API_KEY=process.env.REACT_APP_OPENAI_API_KEY
